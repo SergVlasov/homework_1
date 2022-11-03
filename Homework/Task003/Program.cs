6478691fa0ee8c2a99a07fdc5dc5b6691c1e2b2d
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-03 12:36:32 +0300

    Editing files

--- Homework/Task003/Program.cs
+++ Homework/Task003/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт,  является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Hello, please enter number: ");
+int Number = Int32.Parse (Console.ReadLine());
+string answer;
+if(Number % 2 == 0)
+{
+   answer = ("YES, the number is even");
+}
+else
+{
+    answer = ("NO, the number is odd");
+}
+Console.WriteLine(answer);
