4f6ccf7205f32d148b48f63bae7ac744b81cb8c2
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-03 12:21:07 +0300

    Editing files

--- Homework/Task002/Program.cs
+++ Homework/Task002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Hello, please enter three numbers:");
+int numberA = Int32.Parse(Console.ReadLine());
+int numberB = Int32.Parse(Console.ReadLine());
+int numberC = Int32.Parse(Console.ReadLine());
+int  max = numberA;
+if(numberB > max)
+{
+    max = numberB;
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine($"Num {max} is maximum ");
