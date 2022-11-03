7ab7ed56271f0972a432c1729f7f922d359f0b0a
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-03 12:10:16 +0300

    Editing files

--- Homework/Task001/Program.cs
+++ Homework/Task001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Hello, please enter two numbers:");
+int numberA = Int32.Parse(Console.ReadLine());
+int numberB = Int32.Parse(Console.ReadLine());
+int max=0, min = 0;
+if (numberA>numberB)
+{
+    max = numberA; 
+    min = numberB;
+}
+else
+{
+    max = numberB; 
+    min = numberA;
+}
+Console.WriteLine($"Num {max} is maximum");
+Console.WriteLine($"Num {min} is minimum");
