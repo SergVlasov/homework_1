72a358c5ba607a6d0772ad9e9ca7b1d61b45769a
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-03 13:02:01 +0300

    Editing files

--- Homework/Task004/Program.cs
+++ Homework/Task004/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Hello! Please enter your name: ");
+string name = Console.ReadLine();
+Console.Write("Hello, ");
+Console.WriteLine(name);
+Console.Write(name);
+Console.Write(", please enter number: ");
+int number = Int32.Parse (Console.ReadLine());
+Console.Write("I output all even numbers up to: ");
+Console.WriteLine(number);
+int N = 1;
+int size = number;
+while (N <= size)
+{
+    if (N % 2 == 0)
+    {
+        Console.WriteLine(N); 
+        N = N + 1;
+    }
+    else
+    {
+        N = N + 1;
+    }
+}
